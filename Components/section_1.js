5310779559979a2f5e42d0a08424388acacf8a7b
mahmoudamr5896 <mahmoudamr5896@gmail.com>
2024-05-19 23:48:03 +0300

    Initial commit from Create Next App

---
+++ Components/section_1.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function section_1() {
+  return (
+    <div>section_1</div>
+  )
+}
---
+++ pages/home.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import section_1 from '@/Components/section_1'
+export default function home() {
+  return (
+    <><section_1></section_1><div>home</div></>
+  )
+}
