85889b4b40f12c5933f931cb2b2320ebf5820eab
mahmoudamr5896 <mahmoudamr5896@gmail.com>
2024-08-23 20:48:50 +0300

    kk

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import '@/node_modules/bootstrap/dist/css/bootstrap.min.css'
 import HeroSection from "@/components/Section_2";
-import Specialities from "@/components/Section";
 const inter = Inter({ subsets: ["latin"] });
 
 
